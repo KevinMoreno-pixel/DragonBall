241f875f118964cbd52cd7bbbdf81bac8ce438d3
KevinMoreno-pixel <kevinmorenogg2003@gmail.com>
2025-04-23 09:17:15 -0500

    Configuraion de ErrorPage

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -4,30 +4,24 @@ import Stack from '@mui/material/Stack';
 import AlertTitle from '@mui/material/AlertTitle';
 
 const ErrorPage = () => {
-    const isMobile = useMediaQuery('(max-width:600px)');
 
     return (
         <>
 
-            {isMobile ? (
-                <Stack sx={{ justifyContent: 'start', height: '100vh', alignItems: 'center', overflow: 'hidden', padding: 4, }} spacing={2}>
-
-                    <img src="./loading.gif" alt="" />
 
 
-                </Stack>
 
-            ) : (
 
-                <Stack sx={{ justifyContent: 'start', height: '100vh', alignItems: 'center', overflow: 'hidden', padding: 4, }} spacing={2}>
 
-                    <Alert sx={{ width: '50', }} variant='filled' severity="error"><AlertTitle>Error 404</AlertTitle>
+                <Stack sx={{ justifyContent: 'center', height: '100%', alignItems: 'center', overflow: 'hidden', padding: 4, }} spacing={2}>
+
+                    <img src="./loading.gif" alt="" style={{width:"100vh",height:"100%",}} />
+                    <Alert sx={{ width: '50', }} variant='filled' severity="error"><AlertTitle>
+                          Error 404</AlertTitle>
                         Hay un inconveniente en la ruta</Alert>
 
                 </Stack>
 
-
-            )}
 
 
 
